556b959029f7e9f035ca968b0bf04f52f2c9b6f0
Anup <anupkumar4u@gmail.com>
2017-02-13 16:20:48 +0530

    Calender modified

---
+++ app/src/main/java/com/pathfinder/anup/calendar/CalendarActivity.java
@@ -0,0 +1,52 @@
+package com.pathfinder.anup.calendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.pathfinder.anup.imptodo.R;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+
+/**
+ * Created by Anup on 2/13/2017.
+ */
+
+public class CalendarActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_calendar);
+
+        HashSet<Date> event = new HashSet<>();
+
+        event.add(new Date());
+
+        CalendarView cv = ((CalendarView)findViewById(R.id.calendar_view));
+        cv.updateCalendar(event);
+
+        cv.setEventHandler(new CalendarView.EventHandler() {
+            @Override
+            public void onDayLongPress(Date date) {
+                // show returned day
+                DateFormat df = SimpleDateFormat.getDateInstance();
+                Toast.makeText(CalendarActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cv.setClickHandler(new CalendarView.GridClickHandler() {
+            @Override
+            public void onGridClickListener(Date date) {
+                DateFormat df = SimpleDateFormat.getDateInstance();
+                Toast.makeText(CalendarActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/pathfinder/anup/dailyaction/DailyActivityScreen.java
@@ -0,0 +1,10 @@
+package com.pathfinder.anup.dailyaction;
+
+import android.app.Activity;
+
+/**
+ * Created by Anup on 2/13/2017.
+ */
+
+public class DailyActivityScreen extends Activity {
+}
